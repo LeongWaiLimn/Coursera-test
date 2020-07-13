134a346aec13701567a4b1886809f39ef40eeced
Leong Wailimn <Leong.wailimn@yahoo.com>
2020-07-13 21:58:43 +0800

    learning Js

---
+++ messing_around/learningjavascript.js
@@ -0,0 +1,33 @@
+// const name = "Will";
+// const age = 25;
+
+// //template string
+// console.log(`My name is ${name} and i am ${age}`)
+
+// //methods eg
+// const s = "hello world!";
+
+// console.log (s.length);
+
+
+// const person = {
+// 	firstName: "Will",
+// 	lastName: "Leong",
+// 	age: 25,
+// 	hobbies: ['games','music','sleeping'],
+// 	address: {
+// 		street: 'Tropicana Gardens',
+// 		unit: 'B2-22-11',
+// 		state: 'PJ',
+// 	}
+// }
+
+// person.email = 'leong.wailimn@yahoo.com'
+
+// console.log(person);
+
+//for loops
+
+for(let i = 0; i < 10; i++) {
+	console.log(i)
+}
